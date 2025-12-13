bdde67fac02347774f42cb662a7c0445476de1a5
Traps0106 <rayyankhan2005@gmail.com>
2025-12-13 16:17:54 +0300

    Added final list of sample locations

--- Backend/data/locations.js
+++ Backend/data/locations.js
@@ -1,7 +1,9 @@
 export const predefinedLocations = {
-    "Manama": { lat: 26.2285161, lng: 50.5860497 },
-    "Riffa": { lat: 26.1226062, lng: 50.534262 },
-    "Muharraq": { lat: 26.2572, lng: 50.6119 },
-    "Isa Town": { lat: 26.1656051, lng: 50.5523828 },
-    "American University of Bahrain": { lat: 26.1008, lng: 50.5481 },
+    "AUBH": { lat: 26.1008, lng: 50.5481 },
+    "Juffair": { lat: 26.2172, lng: 50.6100 },
+    "Busaiteen": { lat: 26.2386, lng: 50.6344 },
+    "Aali": { lat: 26.1481, lng: 50.5742 },
+    "KU": { lat: 26.2285, lng: 50.5861 },
+    "Polytechnic": { lat: 26.2050, lng: 50.5600 },
 };
+
